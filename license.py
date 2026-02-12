7233f5663889c01931b75af226e7eac8f7fea37a
aliali <alit40113@gmail.com>
2026-02-12 03:17:01 -0800

    Update license.py

--- license.py
+++ license.py
@@ -16,7 +16,7 @@ def load_db():
             # مفتاح 1: 1234
             "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4": {
                 "name": "Ali Khalaf",
-                "expires": "2026-2-13T23:59:59",
+                "expires": "2026-02-13T23:59:59",
                 "limit": 10000,
                 "hwids": []
             },
